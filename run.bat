80867557385fa736cd00d02f531f2f00d5132aa9
jy7894 <joshua.chitts@gmail.com>
2025-11-25 22:04:02 +1100

    fixed scripts

--- run.bat
+++ run.bat
@@ -1,19 +1,20 @@
 @echo off
-cd /d "%~dp0"
 
-:: Try py first
 py --version >nul 2>&1
 if %errorlevel%==0 (
-    echo Python found using "py"
     set PY_CMD=py
     goto :done
 )
 
-:: Try python next
 python --version >nul 2>&1
 if %errorlevel%==0 (
-    echo Python found using "python"
     set PY_CMD=python
+    goto :done
+)
+
+python3 --version >nul 2>&1
+if %errorlevel%==0 (
+    set PY_CMD=python3
     goto :done
 )
 
@@ -21,8 +22,12 @@ echo No Python installation found.
 exit /b 1
 
 :done
-set projectName=%1
-set language=%2
-set path=%3
+set projectName=%~1
+set language=%~2
+set givenPath=%~3
 
-%PY_CMD% "src/main.py" "%projectName%" "%language%" "%path%"
+if "%givenPath%"=="" (
+    set givenPath=%CD%
+)
+
+"%PY_CMD%" "%~dp0src\main.py" "%projectName%" "%language%" "%givenPath%"
--- run.sh
+++ run.sh
@@ -3,5 +3,27 @@
 projectName=$1
 language=$2
 path=$3
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-python3 src/main.py $projectName $language $path
+if [ -x "$path"]; then 
+    path="$(pwd)"
+fi
+
+if [ -z "$PY_CMD" ] && command -v py >/dev/null 2>&1; then
+    PY_CMD=py
+fi
+
+if [ -z "$PY_CMD" ] && command -v python3 >/dev/null 2>&1; then
+    PY_CMD=python3
+fi
+
+if [ -z "$PY_CMD" ] && command -v python >/dev/null 2>&1; then
+    PY_CMD=python
+fi
+
+if [ -z "$PY_CMD" ]; then
+    echo "No Python installation found."
+    exit 1
+fi
+
+"$PY_CMD" "$SCRIPT_DIR"/src/main.py "$projectName" "$language" "$path"
