0faf8b273f7c8e28166624332c25abff41e37ec6
jy7894 <joshua.chitts@gmail.com>
2025-11-25 14:02:03 +1100

    Cleaned up code and added QOL features

--- src/main.py
+++ src/main.py
@@ -1,69 +1,79 @@
 import sys
 import os
 import venv
-from typing import Union
+import shutil
 
-def Write2File(text: str, file: Union[str, os.PathLike[str]]) -> None:
+def Write2File(text: str, file: str | os.PathLike[str]) -> None:
     try:
         with open(file, 'w') as f:
             f.write(text)
     except OSError as e:
         raise RuntimeError(f"Failed to write to file '{file}': {e}")
+    
+def createMainFile(langSuffix: str, projectPath: str | os.PathLike[str]) -> bool:
+    try:
+        if os.path.exists(projectPath):
+            respone: str = input("This file already exists do you want to overwrite it? [Y/n]")
+            if respone == "" or respone.lower() == "y":
+                shutil.rmtree(projectPath)
+            elif respone.lower() == "n":
+                exit()
+        
+        os.makedirs(projectPath, exist_ok=False)
+        os.chdir(projectPath)
+
+        language, batScript, bashScript = determineLanguage(langSuffix, projectPath)
+
+        if language == "py": venv.EnvBuilder(with_pip=True).create(".venv")
+
+        Write2File(batScript, "run.bat")
+        Write2File(bashScript, "run.bash")
+
+        with open(f"main.{language}",'w') as x: pass
+        print(f"Sucesfully made project at \"{projectPath}\"")
+
+        return True
+    except OSError as e:
+        raise RuntimeError(f"ERROR creating project: {e}")
+
+def determineLanguage(language: str, projectPath: str | os.PathLike[str]) -> tuple[str, str, str]:
+    """
+    takes the langauge and project path and returns the language suffix, a bat script, and a bash script as strings
+    """
+    if language in {"python", "py"}: 
+        Language: str = "py"
+        batScript: str = f"@echo off\r\n\"{projectPath}\\.venv\\Scripts\\python.exe\" \"{projectPath}\\main.py\""
+        bashSctipt: str = f"#!/bin/bash\n{projectPath}/.venv/bin/python {projectPath}/main.py"
+        return Language, batScript, bashSctipt
+    
+    elif language in {"java","jar"}:
+        Language: str = "java"
+        batScript: str = f"@echo off\r\njavac \"{projectPath}\\main.java\"\r\njava \"{projectPath}\\main\""
+        bashSctipt: str = f"#!/bin/bash\njavac {projectPath}/main.java\njava {projectPath}/main.java"
+        return Language, batScript, bashSctipt
+    
+    elif language in {"c++","cpp","cc","cxx"}:
+        Language: str = "cpp"
+        batScript: str = f"@echo off\r\ncd /d \"{projectPath}\"\r\ng++ -o run.exe main.cpp\r\nrun.exe\r\ndel run.exe"
+        bashSctipt: str = f"#!/bin/bash\ncd \"{projectPath}\"\ng++ -o run main.cpp\n./run\nrm run"
+        return Language, batScript, bashSctipt
+    
+    else:
+        shutil.rmtree(projectPath)
+        raise AttributeError("Unsupported Langauge\nLanguage Options: Python, Java, C++")
 
 def main() -> None:
     if len(sys.argv) < 3:
         raise ValueError("Usage: main.py <projectName> <language> <path(OPTIONAL)>\nLanguage Options: Python, Java, C++")
     
     projectName: str = sys.argv[1].strip().lower()
-    langaugePrefix: str = sys.argv[2].strip().lower()
-    givenProjectPath: str = sys.argv[3] if len(sys.argv) > 3 else os.path.dirname(os.path.abspath(__file__))
+    langaugeSuffix: str = sys.argv[2].strip().lower()
+    givenProjectPath: str = sys.argv[3] if len(sys.argv) > 3 else os.getcwd()
+
+    createMainFile(
+        langSuffix=langaugeSuffix,
+        projectPath=os.path.join(givenProjectPath, projectName)
+    )
     
-    if not os.path.exists(givenProjectPath):
-        raise FileNotFoundError(f"Path does not exist {givenProjectPath}")
-
-    project_path = os.path.join(givenProjectPath, projectName)
-
-    os.makedirs(project_path, exist_ok=False)
-    os.chdir(project_path)
-
-    lang: str = None
-
-    if langaugePrefix in {"python","py"}:
-        runScript = f"#!/bin/bash\n{project_path}/.venv/bin/python {project_path}/main.py"
-        batScript = f"@echo off\r\n\"{project_path}\\.venv\\Scripts\\python.exe\" \"{project_path}\\main.py\""
-        lang = "py"
-
-        venv.EnvBuilder(with_pip=True).create(".venv")
-
-        Write2File(batScript, "run.bat")
-        Write2File(runScript, "run.sh")
-        
-    elif langaugePrefix in {"java","jar"}:
-        runScript = f"#!/bin/bash\njavac {project_path}/main.java\njava {project_path}/main.java"
-        batScript = f"@echo off\r\njavac \"{project_path}\\main.java\"\r\njava \"{project_path}\\main\""
-
-        lang = "java"
-
-        Write2File(batScript, "run.bat")
-        Write2File(runScript, "run.sh")
-
-    elif langaugePrefix in {"c++","cpp","cc","cxx"}:
-        runScript = f"#!/bin/bash\ncd \"{project_path}\"\ng++ -o run main.cpp\n./run\nrm run"
-        batScript = f"@echo off\r\ncd /d \"{project_path}\"\r\ng++ -o run.exe main.cpp\r\nrun.exe\r\ndel run.exe"
-
-        lang = "cpp"
-
-        Write2File(batScript, "run.bat")
-        Write2File(runScript, "run.sh")
-
-    else:
-        os.remove(project_path) if os.path.isfile(project_path) else None
-        raise AttributeError("Unsupported Langauge\nLanguage Options: Python, Java, C++")
-
-    with open(f"main.{lang}",'w') as x:
-            pass
-
-    print(f"Sucesfully made project at \"{project_path}\"")
-
 if __name__ == '__main__':
     main()
