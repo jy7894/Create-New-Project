2d0d2f42bca52189f042740ed8f0764888eb46d3
jy7894 <joshua.chitts@gmail.com>
2025-11-26 17:18:55 +1100

    fixed double-clicking run.bat

--- run.bat
+++ run.bat
@@ -1,21 +1,11 @@
 @echo off
 
-py --version >nul 2>&1
-if %errorlevel%==0 (
-    set PY_CMD=py
-    goto :done
-)
-
-python --version >nul 2>&1
-if %errorlevel%==0 (
-    set PY_CMD=python
-    goto :done
-)
-
-python3 --version >nul 2>&1
-if %errorlevel%==0 (
-    set PY_CMD=python3
-    goto :done
+for %%C in (py python python3) do (
+    %%C --version >nul 2>&1
+    if not errorlevel 1 (
+        set PY_CMD=%%C
+        goto :done
+    )
 )
 
 echo No Python installation found.
@@ -26,8 +16,24 @@ set projectName=%~1
 set language=%~2
 set givenPath=%~3
 
+if "%projectName%"=="" (
+    echo projectName:
+    set /p projectName=
+)
+
+if "%language%"=="" (
+    echo language:
+    set /p language=
+)
+
 if "%givenPath%"=="" (
-    set givenPath=%CD%
+    echo path^<Optional^>:
+    set /p givenPath=
+    if "%givenPath%"=="" (
+        set givenPath=%CD%
+    )
 )
 
 "%PY_CMD%" "%~dp0src\main.py" "%projectName%" "%language%" "%givenPath%"
+
+setlocal
