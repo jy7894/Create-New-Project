c791b223e62cf9935a100a4c8d321d3fcf81c46a
jy7894 <joshua.chitts@gmail.com>
2025-11-30 14:43:18 +1100

    fixed install script

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo export PATH="$"PATH:/home/josh/Create-New-Project >> ~/.bash_profile
+if grep -wq export PATH="$"PATH:/home/josh/Create-New-Project your_file.txt; then
+    echo already installed
+else
+    echo export PATH="$"PATH:/home/josh/Create-New-Project >> ~/.bash_profile
+fi
