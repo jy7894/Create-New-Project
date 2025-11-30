6a859c949d3130a19810f018fd8fbd7ece65eb36
jy7894 <joshua.chitts@gmail.com>
2025-11-30 14:44:17 +1100

    fixed formating

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if grep -wq export PATH="$"PATH:/home/josh/Create-New-Project your_file.txt; then
+if grep -wq "export PATH=""$""PATH:/home/josh/Create-New-Project" your_file.txt; then
     echo already installed
 else
     echo export PATH="$"PATH:/home/josh/Create-New-Project >> ~/.bash_profile
